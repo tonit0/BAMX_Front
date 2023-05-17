92f96953a35eaad449b9bd7bd7c389163bfbcca2
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2023-05-16 17:30:51 -0700

    importacion erronea

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     EncabezadoComponent,
-    NavbarComponent
   ],
   imports: [
     CommonModule
